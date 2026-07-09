10907cdef78138e403fa48f86342e0a6972cc81f
Gourav Dutta <gourav.dutta1@gmail.com>
2026-07-09 14:04:46 +0530

    Create 00_Create_Customer_Master_Table.sql

---
+++ sql/00_Create_Customer_Master_Table.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS Customer_Master;
+
+CREATE TABLE Customer_Master (
+    Customer_ID INTEGER,
+    First_Name TEXT,
+    Last_Name TEXT,
+    Email TEXT,
+    Phone_Number TEXT,
+    Date_of_Birth TEXT,
+    Country TEXT,
+    Customer_Status TEXT
+);
